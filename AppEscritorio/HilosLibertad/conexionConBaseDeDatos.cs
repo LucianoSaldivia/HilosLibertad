4222fdb49712df6c1cd7d97547f6f27159a49a74
Pablo Saldivia <saldiviape@gmail.com>
2021-07-30 16:39:44 -0300

    Conexión hecha

--- AppEscritorio/HilosLibertad/conexionConBaseDeDatos.cs
+++ AppEscritorio/HilosLibertad/conexionConBaseDeDatos.cs
@@ -3,11 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data.SqlClient; // librería necesaria para usar SQL
+using System.Configuration;     // Para poder usar el ConfigurationManager
+using System.Data.SqlClient;    // Para tener disponible la clase conexión
+using System.Data;              // 
+/*using System.Data.SqlClient; // librería necesaria para usar SQL*/
 
 namespace HilosLibertad
 {
-    class conexionConBaseDeDatos
+    public class Conexion
+    {
+        // Método que devuelve un objeto de conexión (de tipo SqlConnection)
+        // Método que devuelve una conexión activa a la base de datos, la cual fue definida previamente en las propiedades del proyecto
+        public SqlConnection LeerCadena() {
+
+            // Se crea el objeto de tipo SqlConnection
+            // Dentro del constructor se va a invocar a la variable "única" que guarda la cadena de conexión, creada previamente
+            SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["unica"].ConnectionString);
+
+            // Se agrega una validación:
+            // Si el estado del objeto de conexión es abierto, se cierra.
+            // Si es cerrado, se abre.
+            if (cn.State == ConnectionState.Open) {
+                cn.Close();
+            }
+            else {
+                cn.Open();
+            }
+            
+            // Se retorna el objeto de conexión (esté abierto o cerrado)
+            return cn;
+        }
+    }
+    
+    
+    
+    /*class conexionConBaseDeDatos
     {
         // PABLO-PC             nombre de usuario de la base de datos
         // pruebaDB             nombre de la base de datos
@@ -18,7 +48,6 @@ namespace HilosLibertad
         public conexionConBaseDeDatos() {
             conectarBD.ConnectionString = cadenaDeConexion;
         }
-
 
         public void abrir() {
             try {
@@ -34,5 +63,5 @@ namespace HilosLibertad
             conectarBD.Close();
         }
 
-    }
+    }*/
 }
