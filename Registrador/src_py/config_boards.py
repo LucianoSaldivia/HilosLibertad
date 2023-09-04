f5f2ba2295c82bc6a2d0840aae72898d60f08bac
LucianoSaldivia <lsaldivia@frba.utn.edu.ar>
2023-09-04 20:52:14 -0300

    Corrección insignificante

--- Registrador/src_py/config_boards.py
+++ Registrador/src_py/config_boards.py
@@ -9,7 +9,7 @@ BOARD3 = False
 # Maquinas de cada placa
 step = 1 # auxiliar para formar las listas a partir de range
 BOARD0_MAQ = list( range(1 , 16+step, step) ) if BOARD0 else list()
-BOARD0_MAQ.remove(8) # Bornera sin Máquina conectada
+BOARD0_MAQ.remove(8 ) # Bornera sin Máquina conectada
 BOARD1_MAQ = list( range(17, 32+step, step) ) if BOARD1 else list()
 BOARD1_MAQ.remove(21) # Bornera sin Máquina conectada
 BOARD1_MAQ.remove(32) # Bornera sin Máquina conectada
