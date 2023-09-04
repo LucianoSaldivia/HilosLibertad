b9371a6eca9e3d774abbcb4df07341ce9752ce2f
LucianoSaldivia <lsaldivia@frba.utn.edu.ar>
2023-09-04 20:59:50 -0300

    Ordenamiento del archivo
    Se reordenaron líneas y definiciones de funciones, clases, etc... dentro de sus funciones de test correspondientes.

--- Registrador/src_py/tests_python_stuff.py
+++ Registrador/src_py/tests_python_stuff.py
@@ -37,109 +37,112 @@ def hideConsole():
     # Hide console window
     ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 0)
     
-# Archivos
-log_file_name = "errors.log"
 
-# Posibles errores
-ErrorLogMsg = {
-    "DB_CONN":  "No se pudo conectar con la base de datos.\n",
-    "DB_WRITE": "Conexión con la Base de datos OK, no se pudo escribir (falló un insert/update). Última conexión/escritura OK: ",
-    "USB_CONN": "No se pudo conectar con CH340 (Driver USB de la máquina).\n",
-    "USB_MUTE": "Conexión con el USB OK, pero no se recibieron más datos de la máquina. Último dato recibido: ",
-    "USB_LOST": "Se perdió la conexión con la máquina. Último dato recibido: "
-}
 
-def writeErrorLog(timestamp: datetime, type: str, last_ok_timestamp: datetime = None):
-    """Escribe el log correspondiente al tipo de error pasado por <type>, con el datetime pasado como timestamp.
-    Las opciones para <type> son:
-        "DB_CONN", "DB_WRITE", "USB_CONN", "USB_MUTE", "USB_LOST"    
-        ( ErrorLogMsg.keys() )
-    """
+
+def testWriteErrorLog():
+    # Archivos
+    log_file_name = "errors.log"
+
+    # Posibles errores
+    ErrorLogMsg = {
+        "DB_CONN":  "No se pudo conectar con la base de datos.\n",
+        "DB_WRITE": "Conexión con la Base de datos OK, no se pudo escribir (falló un insert/update). Última conexión/escritura OK: ",
+        "USB_CONN": "No se pudo conectar con CH340 (Driver USB de la máquina).\n",
+        "USB_MUTE": "Conexión con el USB OK, pero no se recibieron más datos de la máquina. Último dato recibido: ",
+        "USB_LOST": "Se perdió la conexión con la máquina. Último dato recibido: "
+    }
+
+    def writeErrorLog(timestamp: datetime, type: str, last_ok_timestamp: datetime = None):
+        """Escribe el log correspondiente al tipo de error pasado por <type>, con el datetime pasado como timestamp.
+        Las opciones para <type> son:
+            "DB_CONN", "DB_WRITE", "USB_CONN", "USB_MUTE", "USB_LOST"    
+            ( ErrorLogMsg.keys() )
+        """
+        
+        if last_ok_timestamp is None:
+            end_log_line = "\n"
+        else:
+            end_log_line = str(last_ok_timestamp) + "\n"
+        
+        log_line = str(timestamp) + " - " + ErrorLogMsg.get(type) + end_log_line
+        
+        # Abro el archivo como "(a)ppend", "over(w)rite", o (x)"create"
+        with open(log_file_name, "a") as fd:
+            fd.write( log_line )
+            print( log_line )
+
+    writeErrorLog( datetime.now(), ErrorLogMsg.get("DB_WRITE"), datetime.now() - timedelta(minutes=5) )
+
+def testSoporteTipoDeRelay():
     
-    if last_ok_timestamp is None:
-        end_log_line = "\n"
-    else:
-        end_log_line = str(last_ok_timestamp) + "\n"
-    
-    log_line = str(timestamp) + " - " + ErrorLogMsg.get(type) + end_log_line
-    
-    # Abro el archivo como "(a)ppend", "over(w)rite", o (x)"create"
-    with open(log_file_name, "a") as fd:
-        fd.write( log_line )
-        print( log_line )
+    # Definición copiada de Registrador.py (COPIARLA NUEVAMENTE en caso de cambiar algo)
+    class State(enum.IntEnum):
+        STOPPED = 0                 # Máquina apagada
+        WORKING = 1                 # Máquina trabajando
 
+    def getSamplesFromFrame1(frame: bytes, timestamp: datetime) -> list: # Original, Funciona pero asume relay Normalmente Abierto
+        """Forma una lista Samples del tipo:
+        0 ->    timestamp (datetime)
+        1 ->    state_MAQ1
+        2 ->    state_MAQ2
+        3 ->    state_MAQ3
+        ...
+        """
 
+        sample = list()
+        sample.append( timestamp )
 
-class State(enum.IntEnum):
-    STOPPED = 0                 # Máquina apagada
-    WORKING = 1                 # Máquina trabajando
+        # Desde el byte menos significativo al más significativo
+        for byte_in_frame in range(7, -1, -1):
+            # Desde el LSB hasta el MSB
+            for bit_in_byte in range(0, 8):
+                # Agrego el valor correspondiente (0 -> STOPPED; 1 -> WORKING)
+                sample.append( State.WORKING 
+                        if ( frame[byte_in_frame] & (0x01 << bit_in_byte) ) 
+                        else State.STOPPED )
+                
+        # Retorno la lista Sample completa
+        return sample
+    def getSamplesFromFrame2(frame: bytes, timestamp: datetime) -> list:
+        """Forma una lista Samples del tipo:
+        0 ->    timestamp (datetime)
+        1 ->    state_MAQ1
+        2 ->    state_MAQ2
+        3 ->    state_MAQ3
+        ...
+        """
 
-def getSamplesFromFrame1(frame: bytes, timestamp: datetime) -> list: # Original, Funciona pero asume relay Normalmente Abierto
-    """Forma una lista Samples del tipo:
-    0 ->    timestamp (datetime)
-    1 ->    state_MAQ1
-    2 ->    state_MAQ2
-    3 ->    state_MAQ3
-    ...
-    """
+        sample = list()
+        sample.append( timestamp )
 
-    sample = list()
-    sample.append( timestamp )
+        # Desde el byte menos significativo al más significativo
+        for byte_in_frame in range(7, -1, -1):
+            # Desde el LSB hasta el MSB
+            for bit_in_byte in range(0, 8):
+                # 1 +                                   -> Pasa la posición del bit (0~63) a idMAQ (1~64)
+                # bit_in_byte + 8 * (7-byte_in_frame)   -> Devuelve la posición del bit (0~63)
+                idMAQ_curr = 1 + bit_in_byte + 8 * (7-byte_in_frame)
 
-    # Desde el byte menos significativo al más significativo
-    for byte_in_frame in range(7, -1, -1):
-        # Desde el LSB hasta el MSB
-        for bit_in_byte in range(0, 8):
-            # Agrego el valor correspondiente (0 -> STOPPED; 1 -> WORKING)
-            sample.append( State.WORKING 
-                    if ( frame[byte_in_frame] & (0x01 << bit_in_byte) ) 
-                    else State.STOPPED )
+                # Estado del bit leído (0 o 1)
+                bit_state = bool((frame[byte_in_frame] & (0x01 << bit_in_byte)) >> bit_in_byte)
+                
+                # Tipo de Relay configurado (NORMAL_ABIERTO o NORMAL_CERRADO)
+                relay_type = bool(config_relay_types.RELAY_TYPE[idMAQ_curr])
+                
+                # Obtengo el estado de la máquina a partir del estado del bit y el tipo de relay
+                estado = ( State.WORKING
+                        if ( bit_state ^ relay_type )
+                        else State.STOPPED )
+                
+                # Agrego el valor correspondiente (0 -> STOPPED; 1 -> WORKING)
+                sample.append( estado )
+
+                # Muestro todo
+                # print(f"idMAQ: {idMAQ_curr:02d} -> Bit: {int(bit_state)} -> Estado: {estado} --- RelayType = {config_relay_types.RELAY_TYPE[idMAQ_curr]}")
             
-    # Retorno la lista Sample completa
-    return sample
-def getSamplesFromFrame2(frame: bytes, timestamp: datetime) -> list:
-    """Forma una lista Samples del tipo:
-    0 ->    timestamp (datetime)
-    1 ->    state_MAQ1
-    2 ->    state_MAQ2
-    3 ->    state_MAQ3
-    ...
-    """
-
-    sample = list()
-    sample.append( timestamp )
-
-    # Desde el byte menos significativo al más significativo
-    for byte_in_frame in range(7, -1, -1):
-        # Desde el LSB hasta el MSB
-        for bit_in_byte in range(0, 8):
-            # 1 +                                   -> Pasa la posición del bit (0~63) a idMAQ (1~64)
-            # bit_in_byte + 8 * (7-byte_in_frame)   -> Devuelve la posición del bit (0~63)
-            idMAQ_curr = 1 + bit_in_byte + 8 * (7-byte_in_frame)
-
-            # Estado del bit leído (0 o 1)
-            bit_state = bool((frame[byte_in_frame] & (0x01 << bit_in_byte)) >> bit_in_byte)
-            
-            # Tipo de Relay configurado (NORMAL_ABIERTO o NORMAL_CERRADO)
-            relay_type = bool(config_relay_types.RELAY_TYPE[idMAQ_curr])
-            
-            # Obtengo el estado de la máquina a partir del estado del bit y el tipo de relay
-            estado = ( State.WORKING
-                    if ( bit_state ^ relay_type )
-                    else State.STOPPED )
-            
-            # Agrego el valor correspondiente (0 -> STOPPED; 1 -> WORKING)
-            sample.append( estado )
-
-            # Muestro todo
-            # print(f"idMAQ: {idMAQ_curr:02d} -> Bit: {int(bit_state)} -> Estado: {estado} --- RelayType = {config_relay_types.RELAY_TYPE[idMAQ_curr]}")
-          
-    # Retorno la lista Sample completa
-    return sample
-
-def main():
-    # writeErrorLog( datetime.now(), ErrorLogMsg.get("DB_WRITE"), datetime.now() - timedelta(minutes=5) )
-
+        # Retorno la lista Sample completa
+        return sample
 
     # Empiezo con un string de la trama recibida (ejemplo) como se imprime en el Registrador
     s = "00000000 00001010 00000000 00001010 00000000 00001010 00000000 00001011 11111111" # El último byte es el CRC !
@@ -172,8 +175,14 @@ def main():
             print(f"\tSample datetime: {estado}")
         else:
             print(f"idMAQ: {i:02d} -> Estado: {estado} --- RelayType = {config_relay_types.RELAY_TYPE[i]}")
+
+
+def main():
+    # testWriteErrorLog()
+    testSoporteTipoDeRelay()
+
     
-    exit()
+    
 
 if __name__ == "__main__":
     main()
