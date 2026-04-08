bed28e3906413c3cf653ef63479e302f5efb79e4
saldiviape <saldiviape@gmail.com>
2026-04-08 14:19:01 -0300

    Cambios de nombres, tras cambios de lugar de máquinas

---
+++ SQL Server/UPDATE 2026-04-08/2026-04-08.sql
@@ -0,0 +1,30 @@
+use DB_HILOS_LIBERTAD
+
+INSERT INTO HL.maquinas VALUES (4, 1, 1008, 'Telar 8', 'Telar 8', 1)
+INSERT INTO HL.maquinas VALUES (5, 4, 4001, 'Urdidora', 'Urdidora', 1)
+UPDATE HL.maquinas SET numeroMaquinaUSUARIO = 2008, nombreMaquinaUSUARIO = 'Cordonera 8', descripcionMaquinaUSUARIO = 'Cordonera 8'  WHERE idMaquina = 6
+UPDATE HL.maquinas SET numeroMaquinaUSUARIO = 2010, nombreMaquinaUSUARIO = 'Cordonera 10', descripcionMaquinaUSUARIO = 'Cordonera 10'  WHERE idMaquina = 7
+UPDATE HL.maquinas SET numeroMaquinaUSUARIO = 2009, nombreMaquinaUSUARIO = 'Cordonera 9', descripcionMaquinaUSUARIO = 'Cordonera 9'    WHERE idMaquina = 8
+INSERT INTO HL.maquinas VALUES (9, 2, 2104, 'Trenzadora 4', 'Trenzadora 4', 1)
+DELETE FROM HL.registros
+DELETE FROM HL.maquinas WHERE idMaquina = 10
+UPDATE HL.maquinas SET numeroMaquinaUSUARIO = 2103, nombreMaquinaUSUARIO = 'Trenzadora 3', descripcionMaquinaUSUARIO = 'Trenzadora 3'  WHERE idMaquina = 11
+UPDATE HL.maquinas SET idSector = 3, numeroMaquinaUSUARIO = 3001, nombreMaquinaUSUARIO = 'Puntera 1', descripcionMaquinaUSUARIO = 'Puntera 1'  WHERE idMaquina = 12
+UPDATE HL.maquinas SET idSector = 3, numeroMaquinaUSUARIO = 3002, nombreMaquinaUSUARIO = 'Puntera 2', descripcionMaquinaUSUARIO = 'Puntera 2'  WHERE idMaquina = 13
+UPDATE HL.maquinas SET numeroMaquinaUSUARIO = 2006, nombreMaquinaUSUARIO = 'Cordonera 6', descripcionMaquinaUSUARIO = 'Cordonera 6'    WHERE idMaquina = 14
+UPDATE HL.maquinas SET numeroMaquinaUSUARIO = 2007, nombreMaquinaUSUARIO = 'Cordonera 7', descripcionMaquinaUSUARIO = 'Cordonera 7'    WHERE idMaquina = 15
+UPDATE HL.maquinas SET numeroMaquinaUSUARIO = 2004, nombreMaquinaUSUARIO = 'Cordonera 4', descripcionMaquinaUSUARIO = 'Cordonera 4'    WHERE idMaquina = 16
+UPDATE HL.maquinas SET numeroMaquinaUSUARIO = 2003, nombreMaquinaUSUARIO = 'Cordonera 3', descripcionMaquinaUSUARIO = 'Cordonera 3'    WHERE idMaquina = 17
+UPDATE HL.maquinas SET numeroMaquinaUSUARIO = 2002, nombreMaquinaUSUARIO = 'Cordonera 2', descripcionMaquinaUSUARIO = 'Cordonera 2'    WHERE idMaquina = 18
+UPDATE HL.maquinas SET numeroMaquinaUSUARIO = 2001, nombreMaquinaUSUARIO = 'Cordonera 1', descripcionMaquinaUSUARIO = 'Cordonera 1'    WHERE idMaquina = 19
+UPDATE HL.maquinas SET numeroMaquinaUSUARIO = 2102, nombreMaquinaUSUARIO = 'Trenzadora 2', descripcionMaquinaUSUARIO = 'Trenzadora 2'    WHERE idMaquina = 20
+UPDATE HL.maquinas SET numeroMaquinaUSUARIO = 2101, nombreMaquinaUSUARIO = 'Trenzadora 1', descripcionMaquinaUSUARIO = 'Trenzadora 1'    WHERE idMaquina = 21
+UPDATE HL.maquinas SET idSector = 1, numeroMaquinaUSUARIO = 1009, nombreMaquinaUSUARIO = 'Telar 9', descripcionMaquinaUSUARIO = 'Telar 9'  WHERE idMaquina = 24
+UPDATE HL.maquinas SET idSector = 1, numeroMaquinaUSUARIO = 1007, nombreMaquinaUSUARIO = 'Telar 7', descripcionMaquinaUSUARIO = 'Telar 7'  WHERE idMaquina = 25
+UPDATE HL.maquinas SET idSector = 1, numeroMaquinaUSUARIO = 1006, nombreMaquinaUSUARIO = 'Telar 6', descripcionMaquinaUSUARIO = 'Telar 6'  WHERE idMaquina = 26
+UPDATE HL.maquinas SET idSector = 1, numeroMaquinaUSUARIO = 1005, nombreMaquinaUSUARIO = 'Telar 5', descripcionMaquinaUSUARIO = 'Telar 5'  WHERE idMaquina = 27
+UPDATE HL.maquinas SET idSector = 1, numeroMaquinaUSUARIO = 1004, nombreMaquinaUSUARIO = 'Telar 4', descripcionMaquinaUSUARIO = 'Telar 4'  WHERE idMaquina = 28
+UPDATE HL.maquinas SET idSector = 1, numeroMaquinaUSUARIO = 1003, nombreMaquinaUSUARIO = 'Telar 3', descripcionMaquinaUSUARIO = 'Telar 3'  WHERE idMaquina = 29
+UPDATE HL.maquinas SET idSector = 1, numeroMaquinaUSUARIO = 1002, nombreMaquinaUSUARIO = 'Telar 2', descripcionMaquinaUSUARIO = 'Telar 2'  WHERE idMaquina = 30
+UPDATE HL.maquinas SET idSector = 1, numeroMaquinaUSUARIO = 1001, nombreMaquinaUSUARIO = 'Telar 1', descripcionMaquinaUSUARIO = 'Telar 1'  WHERE idMaquina = 31
+
