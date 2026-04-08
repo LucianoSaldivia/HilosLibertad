eff25f5f3ec2f65c80879f7fe1573ad080d4af2a
saldiviape <saldiviape@gmail.com>
2026-04-08 13:33:20 -0300

    Config boards actualizadas
    Máquinas de cada placa registrada con un barrido nuevo hecho hoy, TODO como quedó registrado en el spreadsheet "Hilos Libertad PLACAS y BORNERAS".

--- Registrador/src_py/config_boards.py
+++ Registrador/src_py/config_boards.py
@@ -8,16 +8,17 @@ BOARD3 = False
 
 # Maquinas de cada placa
 step = 1 # auxiliar para formar las listas a partir de range
-# IdMaq = #Bornera en placa
+###################################################
+###################################################
+##########  IdMaq = #Bornera en placa  ############
+###################################################
+###################################################
 BOARD0_MAQ = list( range( 0, 16, step) ) if BOARD0 else list()
 BOARD0_MAQ.remove( 0) # Bornera sin Máquina conectada
 BOARD0_MAQ.remove( 1) # Bornera sin Máquina conectada
 BOARD0_MAQ.remove( 2) # Bornera sin Máquina conectada
 BOARD0_MAQ.remove( 3) # Bornera sin Máquina conectada
-BOARD0_MAQ.remove( 4) # Bornera sin Máquina conectada
-BOARD0_MAQ.remove( 5) # Bornera sin Máquina conectada
-# IdMaq (= #Bornera) 6, 7 y 8 asignadas
-BOARD0_MAQ.remove( 9) # Bornera sin Máquina conectada
+BOARD0_MAQ.remove(10) # Bornera sin Máquina conectada
 BOARD1_MAQ = list( range(16, 32, step) ) if BOARD1 else list()
 BOARD1_MAQ.remove(22) # Bornera sin Máquina conectada
 BOARD1_MAQ.remove(23) # Bornera sin Máquina conectada
